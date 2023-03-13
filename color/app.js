855069f6deda6097489c193a62039e77ce670e68
Noam Wolf <noamyonah@gmail.com>
2023-03-13 10:32:33 -0400

    tweak app.js

---
+++ color/app.js
@@ -0,0 +1,22 @@
+const classes = ['red', 'green', 'purple']
+let color = 0;
+const heading = document.getElementById('heading')
+
+heading.addEventListener("click", changeColor);
+document.addEventListener("click", function()
+{
+    console.log(color)
+    if (color < classes.length - 1)
+    {
+        color++
+    }
+    else
+    {
+        color = 0
+    }
+})
+
+function changeColor()
+{
+    heading.style.color = classes[color]
+}
